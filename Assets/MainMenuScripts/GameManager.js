8e61af577a106a8fda562ea060c6ae906f7d0f16
TimothySeah <timothy.seah777@yahoo.com>
2015-10-12 15:24:38 -0400

    everything
    everything

---
+++ Assets/MainMenuScripts/GameManager.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+var playerMove : Component;
+var crashed : boolean = false;
+var finalTime : float;
+var width : int;
+
+function Start() {
+}
+
+function OnGUI () {
+	//restart game when pressed
+	if (crashed)
+		if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "Restart")) Application.LoadLevel("MainScene");
+	
+	//display elapsed time
+	var elapsed : int;
+	if (crashed) elapsed = finalTime;
+	else elapsed = Time.timeSinceLevelLoad;
+	GUI.TextField(Rect(0, 0, 80, 20), "" + elapsed);
+	GUI.TextField(Rect(Screen.width - 80, 0, 80, 20), "" + PlayerPrefs.GetInt("high_score"));
+	
+}
+
+//do stuff when player collides
+function triggered() {
+	crashed = true;
+	finalTime = Time.timeSinceLevelLoad; //stop timer
+	
+	//save high score 
+	if (finalTime > PlayerPrefs.GetInt("high_score")) {
+		PlayerPrefs.SetInt("high_score", finalTime);
+		PlayerPrefs.Save();
+	}
+}
---
+++ Assets/MainSceneScripts/PlayerMovement.js
@@ -0,0 +1,53 @@
+﻿#pragma strict
+
+var block : GameObject; //prefab block
+var gui : GameObject; //the gui handler
+
+var initialSpeed : float = 5; //speed of character
+var canMove : boolean = true;
+
+//determines blockmaking speed
+var timeSince : float = -3;
+var creationInterval : float = 0.5;
+
+function Start () {
+	InvokeRepeating("makeBlock", 3, 0.5);
+}
+
+function Update () {
+	if (canMove) {
+		//constantly move player forward
+		transform.Translate(0, 0, Time.deltaTime * initialSpeed + 0.01 * Time.timeSinceLevelLoad);
+	
+		//let player move left/right
+		if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(Time.deltaTime * -10, 0, 0);
+		if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(Time.deltaTime * 10, 0, 0);
+	}
+	
+	
+	//increase blockmaking over time 
+	timeSince += Time.deltaTime;
+	if (timeSince > creationInterval) {
+		makeBlock();
+		//mod for future
+		timeSince = 0;
+		creationInterval /= 1.01;
+	}
+	
+}
+
+
+
+//instantiate a random block
+function makeBlock() {
+	if (canMove)
+		Instantiate(block, new Vector3(transform.position.x + Random.Range(-10, 10), transform.position.y, transform.position.z + Random.Range(8, 12)), transform.rotation); 
+}
+
+//do stuff upon crashing
+function OnTriggerEnter(other : Collider) {
+	canMove = false; //stop movement
+	
+	//alert gui to make appropriate changes
+	gui.SendMessage("triggered");
+}
